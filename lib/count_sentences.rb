ff72d859b160621d50ffdf41bbd0568227628849
ren831 <rene83120@gmail.com>
2022-05-09 11:13:26 -0700

    commit tree 771a9cff2f29830c905eb0b072666d49ce68ad1e

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,15 +3,15 @@ require 'pry'
 class String
 
   def sentence?
-    
+     end_with?(".")
   end
 
   def question?
-
+  end_with?("?")
   end
 
   def exclamation?
-
+  end_with?("!")
   end
 
   def count_sentences
